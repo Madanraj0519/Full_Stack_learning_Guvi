bb9d03b827ae2e35dcab861c8ea97d4431e5c4b5
Madan raj <125730836+Madanraj0519@users.noreply.github.com>
2024-01-07 00:25:04 +0530

    "resolved error"

--- Day-16/react-router/src/components/Blogs/blogs.jsx
+++ Day-16/react-router/src/components/Blogs/blogs.jsx
@@ -7,7 +7,7 @@ const blogs = (props) => {
      <>
   <div class="col">
     <div class="card h-100">
-      <img src={blog.imageUrl}class="card-img-top pointer" alt={blog.title}/>
+      <img src={blog.imageUrl}class="card-img-top pointer" alt="image"/>
       <div class="card-body px-4">
         <h5 class="card-title text-start">{blog.title}</h5>
         <p class="card-text text-start">{blog.text}</p>
