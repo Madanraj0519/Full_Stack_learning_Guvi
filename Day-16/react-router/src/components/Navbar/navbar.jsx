3ae9662545b616cad26450e4d24ecf7dc99a30f8
Madan raj <125730836+Madanraj0519@users.noreply.github.com>
2024-01-07 00:20:48 +0530

    Added task16

--- Day-16/react-router/src/components/Navbar/navbar.jsx
+++ Day-16/react-router/src/components/Navbar/navbar.jsx
@@ -5,8 +5,9 @@ const navBar = () => {
     return (
         <div className="container">
             <nav class="navbar navbar-expand-lg  ">
-                <Link className='navbar-brand' to="/">
-                <img src='https://www.guvi.in/blog/wp-content/uploads/2022/04/Guvi-blog-logo.png'/>
+                <Link className='navbar-brand d-flex align-items-center gap-2' to="/">
+                   <img className='image-poster'src='https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_b2b52bf26a769b861fae19c5f65643cf/guvi.png' alt='guvi-logo'/>
+                   <h4>Blogs</h4>
                 </Link>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navBarTop" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
