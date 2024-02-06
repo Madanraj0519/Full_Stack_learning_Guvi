e5eceb0d0cd61b41222883a3bd6539c0a85330c9
Madan raj <125730836+Madanraj0519@users.noreply.github.com>
2024-02-06 11:18:17 +0530

    Added new items in the folders

--- Day-26/Bookinghall/dbConfig.js
+++ Day-26/Bookinghall/dbConfig.js
@@ -1,8 +1,12 @@
 const { default: mongoose } = require("mongoose");
+require('dotenv').config()
 
 async function connectDatabase(){
     try{
-        const response = await mongoose.connect("mongodb://127.0.0.1:27017/bookings");
+        const response = await mongoose.connect(`${ 
+            process.env.Node_Env !== 'development' ? 
+            process.env.MONGODB_LOCAL_URL : process.env.MONGODB_ATLAS_URL}
+            `);
         if(response.connections.length > 0){
             console.log('Database connection successfully established');
         }else{
